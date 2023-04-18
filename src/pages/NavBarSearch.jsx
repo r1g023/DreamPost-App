ae7a9962bee42d97d303f80109f26ecd49b9ecc1
r1g023 <limon1285@gmail.com>
2023-04-17 23:39:35 -0500

    fix styling for avatar, adjust searchbar on mobile/desktop devices

--- src/pages/NavBarSearch.jsx
+++ src/pages/NavBarSearch.jsx
@@ -24,7 +24,7 @@ const NavBarSearch = ({
 }) => {
   return (
     <Search
-      className="searchBar responsive"
+      className={errorMessage ? "searchError" : "searchBar responsive"}
       sx={{
         position: "fixed",
         marginTop: "19px",
